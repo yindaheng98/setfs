1a813ea70fa07e74849962d8c23122c31eddf7c8
yindaheng98 <yindaheng98@163.com>
2021-05-21 18:12:41 +0800

    基本结构

--- src/main.c
+++ src/main.c
@@ -1,10 +1,55 @@
-#include <config.h>
-#include <stdio.h>
+/*
+  Big Brother File System
+  Copyright (C) 2012 Joseph J. Pfeiffer, Jr., Ph.D. <pfeiffer@cs.nmsu.edu>
 
-int
-main (void)
+  This program can be distributed under the terms of the GNU GPLv3.
+  See the file COPYING.
+
+  This code is derived from function prototypes found /usr/include/fuse/fuse.h
+  Copyright (C) 2001-2007  Miklos Szeredi <miklos@szeredi.hu>
+  His code is licensed under the LGPLv2.
+  A copy of that code is included in the file fuse.h
+  
+  The point of this FUSE filesystem is to provide an introduction to
+  FUSE.  It was my first FUSE filesystem as I got to know the
+  software; hopefully, the comments in this code will help people who
+  follow later to get a gentler introduction.
+
+  This might be called a no-op filesystem:  it doesn't impose
+  filesystem semantics on top of any other existing structure.  It
+  simply reports the requests that come in, and passes them to an
+  underlying filesystem.  The information is saved in a logfile named
+  bbfs.log, in the directory from which you run bbfs.
+*/
+#include "config.h"
+
+#include <ctype.h>
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <fuse.h>
+#include <libgen.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#ifdef HAVE_SYS_XATTR_H
+#include <sys/xattr.h>
+#endif
+
+struct fuse_operations setfs_oper = {};
+
+int main(int argc, char *argv[])
 {
-  puts ("Hello World!");
-  puts ("This is " PACKAGE_STRING ".");
-  return 0;
+    int fuse_stat;
+
+    // turn over control to fuse
+    fprintf(stderr, "about to call fuse_main\n");
+    fuse_stat = fuse_main(argc, argv, &setfs_oper);
+    fprintf(stderr, "fuse_main returned %d\n", fuse_stat);
+
+    return fuse_stat;
 }
