fddb45e7a1023d9413127d94f3492f411e5077da
yindaheng98 <yindaheng98@163.com>
2021-05-21 19:06:32 +0800

    simplify headers

--- src/main.c
+++ src/main.c
@@ -24,18 +24,8 @@
 #include "config.h"
 #define FUSE_USE_VERSION 26
 
-#include <ctype.h>
-#include <dirent.h>
-#include <errno.h>
-#include <fcntl.h>
 #include <fuse.h>
-#include <libgen.h>
-#include <limits.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
 #ifdef HAVE_SYS_XATTR_H
 #include <sys/xattr.h>
 #endif
